0c9c448a225c6614dd20b5554455cd40e5cfd5d2
lijianran <lijianran@outlook.com>
2019-03-18 16:46:27 +0800

    test 冲突

--- demo/manage.py
+++ demo/manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
+	print("hello")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
     try:
         from django.core.management import execute_from_command_line
